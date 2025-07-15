bdd77fc80925479830fd58cc37826385da27df9b
Tsie Masilo <tsiemasilo@gmail.com>
2025-07-15 20:01:12 +0200

    Add client/src/pages/chat.tsx

---
+++ client/src/pages/chat.tsx
@@ -0,0 +1,9 @@
+import { ChatInterface } from '@/components/chat/ChatInterface';
+
+export default function Chat() {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+      <ChatInterface />
+    </div>
+  );
+}
